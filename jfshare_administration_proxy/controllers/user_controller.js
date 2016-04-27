08b234e5c0d78421eacaf1d32837c200055c621b
huapengpeng <544523780@qq.com>
2016-04-27 16:56:59 +0800

    Default

--- jfshare_administration_proxy/controllers/user_controller.js
+++ jfshare_administration_proxy/controllers/user_controller.js
@@ -11,6 +11,53 @@ var logger = log4node.configlog4node.useLog4js( log4node.configlog4node.log4jsCo
 
 var User = require('../lib/models/user');
 
+//注册
+router.post('/regist', function(request, respone, next) {
+    var result = {code: 200};
+
+    try{
+        //var arg = request.query;
+        var arg = request.body;
+        logger.info("请求参数错误:" + JSON.stringify(arg));
+        //参数验证
+        if(arg == null || arg.username == null){
+            result.code = 500;
+            result.desc = "请求参数错误";
+            respone.json(result);
+            return;
+        }
+        if(arg == null || arg.pwdEnc == null ||arg.pwdEnc ==""){
+            result.code = 500;
+            result.desc = "请求参数错误";
+            respone.json(result);
+            return;
+        }
+        result.userId=5;
+        respone.json(result);
+
+    } catch (ex) {
+        logger.error("login error:" + ex);
+        result.code = 500;
+        result.desc = "请求参数错误";
+        respone.json(result);
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //登录
 router.post('/login', function(request, respone, next) {
     var result = {code: 200};
