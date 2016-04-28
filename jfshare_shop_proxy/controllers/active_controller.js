91d247d76dd902b3c76bc1c22eb3cbc1e52f202d
YinBo <yinbo@Feinno.com>
2016-04-28 18:36:55 +0800

    更新了数据

--- jfshare_shop_proxy/controllers/active_controller.js
+++ jfshare_shop_proxy/controllers/active_controller.js
@@ -69,14 +69,63 @@ router.get('/messageList', function(request, response, next) {
 
         /************************************测试数据*****************************************/
         var messageList = [2];
-        var  message= {
-            title:"我是标题",
-            content:"我是消息内容",
-            pushDate:"2016-4-27 13:55:26"
-        };
+        /***
+         *
+         *
+         *
+         *
+         * id:int//消息id
+         title: string //标题
+         content: string //内容
+         beginDate: string //开始时间（yyyy-mm-dd）
+         endDate: string //结束时间(yyyy-mm-dd)
+         createTime:string//创建时间(格式如2016-04-25 14:51:55)
 
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         *
+         * **/
+        var  message= {
+            id:1,
+            title:"我是标题",
+            content:"我是消息内容2",
+            beginDate:"2016-4-27 13:51:26",
+            endDate:"2016-4-28 13:52:26",
+            createTime:"2016-3-28 13:56:26"
+        };
+        var  message1= {
+            id:2,
+            title:"我是标题2",
+            content:"我是消息内容",
+            beginDate:"2016-4-27 13:55:29",
+            endDate:"2016-4-28 13:51:26",
+            createTime:"2016-3-28 13:55:26"
+        };
+        var  message2= {
+            id:3,
+            title:"我是标题21",
+            content:"我是消息内容21",
+            beginDate:"2016-4-27 13:53:26",
+            endDate:"2016-4-28 13:55:29",
+            createTime:"2016-3-28 13:54:26"
+        };
+        var  message3= {
+            id:4,
+            title:"我是标题4",
+            content:"我是消息内容4",
+            beginDate:"2016-4-27 13:55:26",
+            endDate:"2016-4-28 13:55:26",
+            createTime:"2016-3-28 13:55:26"
+        };
         messageList[0] = message;
-        messageList[1] = message;
+        messageList[1] = message1;
+        messageList[3] = message2;
+        messageList[2] = message3;
         resContent.messageList = messageList;
         logger.info("===============" + messageList);
         response.json(resContent);
