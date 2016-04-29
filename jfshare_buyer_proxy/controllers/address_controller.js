5b6f167f2fd94eae09923c043fa7745b9b264d33
huapengpeng <544523780@qq.com>
2016-04-29 11:47:49 +0800

    Default

--- jfshare_buyer_proxy/controllers/address_controller.js
+++ jfshare_buyer_proxy/controllers/address_controller.js
@@ -9,7 +9,6 @@ var logger = log4node.configlog4node.useLog4js( log4node.configlog4node.log4jsCo
 
 var Product = require('../lib/models/product');
 var Address = require('../lib/models/address');
-
 //新增收货地址
 router.post('/add', function(req, res, next) {
     var result = {code: 200};
@@ -191,4 +190,84 @@ router.post('/list', function(req, res, next) {
     }
 });
 
+//查询全国省份列表
+router.post('/getprovinces', function(req, res, next) {
+    var result = {code: 200};
+    try{
+
+        Address.getProvinces(function(err, data) {
+            if(err){
+                res.json(err);
+                return;
+            }
+            result.provicnceList = data;
+            res.json(result);
+            logger.info("get provinces response:" + JSON.stringify(result));
+        });
+    } catch (ex) {
+        logger.error("get provinces error:" + ex);
+        result.code = 500;
+        result.desc = "获取省份列表失败";
+        res.json(result);
+    }
+});
+
+//查询省份下的市
+router.post('/getcitys', function(req, res, next) {
+    var result = {code: 200};
+
+    //var provinceId = req.query;
+    var params = req.body;
+    if(params.provinceId<=0 || params.provinceId =="" || params.provinceId==null){
+        result.code = 500;
+        result.desc = "参数错误";
+        res.json(result);
+    }
+    try{
+
+        Address.getCitys(params,function(err, data) {
+            if(err){
+                res.json(err);
+                return;
+            }
+            result.cityList = data;
+            res.json(result);
+            logger.info("get cityList response:" + JSON.stringify(result));
+        });
+    } catch (ex) {
+        logger.error("get cityList error:" + ex);
+        result.code = 500;
+        result.desc = "获取市列表失败";
+        res.json(result);
+    }
+});
+//查询市下面的乡镇
+router.post('/getcountys', function(req, res, next) {
+    var result = {code: 200};
+
+    var params = req.body;
+
+    if(params.cityId<=0 || params.cityId =="" || params.cityId==null){
+        result.code = 500;
+        result.desc = "参数错误";
+        res.json(result);
+    }
+    try{
+
+        Address.getCountys(params,function(err, data) {
+            if(err){
+                res.json(err);
+                return;
+            }
+            result.countyList = data;
+            res.json(result);
+            logger.info("get countyList response:" + JSON.stringify(result));
+        });
+    } catch (ex) {
+        logger.error("get countyList error:" + ex);
+        result.code = 500;
+        result.desc = "获取乡镇列表失败";
+        res.json(result);
+    }
+});
 module.exports = router;
