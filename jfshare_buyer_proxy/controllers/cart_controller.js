089979465fb8d257519aeeba267415d246e4e8d7
YinBo <yinbo@Feinno.com>
2016-05-12 16:52:59 +0800

    商品

--- jfshare_buyer_proxy/controllers/cart_controller.js
+++ jfshare_buyer_proxy/controllers/cart_controller.js
@@ -189,7 +189,7 @@ router.post('/delete', function(req, res, next) {
 //            response.json(err);
 //            return;
 //        }
-////        logger.info("请求的参数，arg：" + JSON.stringify(arg));
+        logger.info("请求的参数，arg：" + JSON.stringify(arg));
         Cart.deleteCartItem(arg, function(err, data){
             if(err) {
                 res.json(err);
