0b5aba3797fd795f04bdbf7382a43881ced2a49b
huapengpeng <544523780@qq.com>
2016-05-13 11:05:12 +0800

    123

--- jfshare_seller_proxy/lib/models/user.js
+++ jfshare_seller_proxy/lib/models/user.js
@@ -37,7 +37,7 @@ User.prototype.login = function(param,callback){
 
 
     //获取客户端
-    var userServ = new Lich.InvokeBag(Lich.ServiceKey.UserServer,'signin',[User,LoginLog]);
+    var userServ = new Lich.InvokeBag(Lich.ServiceKey.SellerServer,'signin',[User,LoginLog]);
     Lich.wicca.invokeClient(userServ, function(err, data){
         logger.info("isLoginNameExist result:" + JSON.stringify(data));
         var res = {};
