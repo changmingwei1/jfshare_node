884e50f03603f5482b900a8671ee2e21de3200c9
larson <lileixun@gmail.com>
2016-05-22 11:18:45 +0800

    缺失

--- jfshare_thirdparty_notify/controllers/pay_controller.js
+++ jfshare_thirdparty_notify/controllers/pay_controller.js
@@ -111,5 +111,188 @@ router.post('/notify/jf189', function(req, res, next) {
     }
 });
 
+router.get('/notify/alipay', function(req, res, next) {
+    try {
+        var ret = {};
+        var arg = req.query;
+        logger.info("########get接收到第三方支付[alipay]的通知【注：仅用于测试模拟支付使用】：" + JSON.stringify(arg));
+        ret.status = 200;
+        ret.msg = "测试成功";
+    } catch (err) {
+        logger.error("调用payServ-payNotify接收支付通知失败！", err);
+        ret.status = 500;
+        ret.msg = "处理失败！";
+    }
+
+    res.json(ret);
+});
+
+router.post('/notify/alipay', function(req, res, next) {
+    try {
+        var ret = {};
+        var arg = req.body;
+        var resObj = JSON.stringify(arg);
+        logger.info("########post接收到第三方支付[alipay]的通知：" + resObj);
+
+        if (paramValid.empty(resObj)) {
+            logger.warn("获取支付通知参数有误, arg");
+            ret.status = 500;
+            ret.msg = "非法参数请求！";
+            res.json(ret);
+            return;
+        }
+
+        var payRes = new pay_types.PayRes({
+            payChannel:2,
+            resUrl:resObj,
+        });
+        // 获取client
+        var payServ = new Lich.InvokeBag(Lich.ServiceKey.PayServer, "payNotify", payRes);
+        Lich.wicca.invokeClient(payServ, function (err, data) {
+            if (err || data[0].result.code == "1") {
+                logger.error("调用payServ-payNotify接收支付通知失败  失败原因 ======" + err);
+                ret.status = 501;
+                ret.msg = "通知失败！";
+                res.json(404);
+                return;
+            }
+
+            logger.info("调用payServ-payNotify接收支付通知成功  result.code =  （" + data[0].result.code + "）  1为失败 0为成功");
+            //logger.info("接口返回数据=====" + data[0]);
+            ret.status = 200;
+            ret.msg = "请求成功";
+            //ret.data = data[0].value;
+            res.json(200);
+        });
+    } catch (err) {
+        logger.error("调用payServ-payNotify接收支付通知失败！", err);
+        ret.status = 500;
+        ret.msg = "处理失败！";
+        res.json(404);
+    }
+});
+
+
+router.get('/notify/weixinpay', function(req, res, next) {
+    try {
+        var ret = {};
+        var arg = req.query;
+        logger.info("########get接收到第三方支付[weixinpay]的通知【注：仅用于测试模拟支付使用】：" + JSON.stringify(arg));
+        ret.status = 200;
+        ret.msg = "测试成功";
+    } catch (err) {
+        logger.error("调用payServ-payNotify接收支付通知失败！", err);
+        ret.status = 500;
+        ret.msg = "处理失败！";
+    }
+
+    res.json(ret);
+});
+
+router.post('/notify/weixinpay', function(req, res, next) {
+    try {
+        var ret = {};
+        var arg = req.body;
+        var resObj = JSON.stringify(arg);
+        logger.info("########post接收到第三方支付[weixinpay]的通知：" + resObj);
+
+        if (paramValid.empty(resObj)) {
+            logger.warn("获取支付通知参数有误, arg");
+            ret.status = 500;
+            ret.msg = "非法参数请求！";
+            res.writeHead(ret.status, {'Content-Type': 'application/xml;charset=utf-8"'});
+            var data  = "\<xml><return_code>FAIL</return_code><return_msg>" + ret.msg + "</return_msg></xml>";
+            res.end(data);
+            return;
+        }
+
+        var payRes = new pay_types.PayRes({
+            payChannel:3,
+            resUrl:resObj,
+        });
+        // 获取client
+        var payServ = new Lich.InvokeBag(Lich.ServiceKey.PayServer, "payNotify", payRes);
+        Lich.wicca.invokeClient(payServ, function (err, data) {
+            if (err || data[0].result.code == "1") {
+                logger.error("调用payServ-payNotify接收支付通知失败  失败原因 ======" + err);
+                ret.status = 501;
+                ret.msg = "通知失败！";
+                //res.json(404);
+                res.writeHead(ret.status, {'Content-Type': 'application/xml;charset=utf-8"'});
+                var data  = "\<xml><return_code>FAIL</return_code><return_msg>" + ret.msg + "</return_msg></xml>";
+                res.end(data);
+                return;
+            }
+
+            logger.info("调用payServ-payNotify接收支付通知成功  result.code =  （" + data[0].result.code + "）  1为失败 0为成功");
+            //logger.info("接口返回数据=====" + data[0]);
+            ret.status = 200;
+            ret.msg = "请求成功";
+            //ret.data = data[0].value;
+            //res.json(200);
+            res.writeHead(ret.status, {'Content-Type': 'application/xml;charset=utf-8"'});
+            var data  = "\<xml><return_code>SUCCESS</return_code><return_msg>OK</return_msg></xml>";
+            res.end(data);
+
+        });
+    } catch (err) {
+        logger.error("调用payServ-payNotify接收支付通知失败！", err);
+        ret.status = 500;
+        ret.msg = "处理失败！";
+        //res.json(404);
+        res.writeHead(ret.status, {'Content-Type': 'application/xml;charset=utf-8"'});
+        var data  = "\<xml><return_code>FAIL</return_code><return_msg>" + ret.msg + "</return_msg></xml>";
+        res.end(data);
+    }
+});
+
+router.get('/notify/hebaopay', function(req, res, next) {
+    try {
+        var ret = {};
+        var arg = req.query;
+        logger.info("########get接收到第三方支付[hebaopay]的通知【注：仅用于测试模拟支付使用】：" + JSON.stringify(arg));
+        ret.status = 200;
+        ret.msg = "测试成功";
+    } catch (err) {
+        logger.error("调用payServ-payNotify接收支付通知失败！", err);
+        ret.status = 500;
+        ret.msg = "处理失败！";
+    }
+
+    res.json(ret);
+});
+
+router.get('/notify/hebaopay', function(req, res, next) {
+    try {
+        var ret = {};
+        var arg = req.query;
+        logger.info("########get接收到第三方支付[hebaopay]的通知【注：仅用于测试模拟支付使用】：" + JSON.stringify(arg));
+        ret.status = 200;
+        ret.msg = "测试成功";
+    } catch (err) {
+        logger.error("调用payServ-payNotify接收支付通知失败！", err);
+        ret.status = 500;
+        ret.msg = "处理失败！";
+    }
+
+    res.json(ret);
+});
+
+router.post('/notify/hebaopay', function(req, res, next) {
+    try {
+        var ret = {};
+        var arg = req.query;
+        logger.info("########post接收到第三方支付[hebaopay]的通知【注：仅用于测试模拟支付使用】：" + JSON.stringify(arg));
+        ret.status = 200;
+        ret.msg = "测试成功";
+    } catch (err) {
+        logger.error("调用payServ-payNotify接收支付通知失败！", err);
+        ret.status = 500;
+        ret.msg = "处理失败！";
+    }
+
+    res.json(ret);
+});
+
 //暴露模块
 module.exports = router;
