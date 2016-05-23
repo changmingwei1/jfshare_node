c6d17afce2fac1a93c56a3c0692ec673df0c0a14
huapengpeng <544523780@qq.com>
2016-05-23 13:29:19 +0800

    123

--- jfshare_seller_proxy/controllers/product_controller.js
+++ jfshare_seller_proxy/controllers/product_controller.js
@@ -807,7 +807,7 @@ router.post('/apply', function (request, response, next) {
 
         //参数校验
         //参数验证
-        if (params.sellerId == null || params.sellerId == "" || params.sellerId <= 0) {
+        if (params.sellerId == null ||  params.sellerId <= 0) {
 
             result.code = 500;
             result.desc = "请求参数错误";
@@ -815,7 +815,7 @@ router.post('/apply', function (request, response, next) {
             return;
         }
 
-        if (params.productId == null || params.productId == "") {
+        if (params.productId == null ) {
 
             result.code = 500;
             result.desc = "请求参数错误";
@@ -823,14 +823,14 @@ router.post('/apply', function (request, response, next) {
             return;
         }
 
-        if (params.state == null || params.state == "") {
+        if (params.state == null) {
 
             result.code = 500;
             result.desc = "请求参数错误";
             response.json(result);
             return;
         }
-        if (params.curState == null || params.curState == "") {
+        if (params.curState == null) {
 
             result.code = 500;
             result.desc = "请求参数错误";
