693c3a7c87fc88980f13f0acb276ad690218eb0e
huapengpeng <544523780@qq.com>
2016-05-24 11:55:07 +0800

    123

--- jfshare_administration_proxy/lib/models/product.js
+++ jfshare_administration_proxy/lib/models/product.js
@@ -786,8 +786,6 @@ Product.prototype.setProductState = function (params, callback) {
 };
 //获取虚拟订单的卡密信息
 Product.prototype.queryProductCard = function (params, callback) {
-    //
-
     var ProductCardParam = new product_types.ProductCardParam({
         transactionId:params.orderId,
     });
