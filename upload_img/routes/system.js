db3f3b0390fd39acbd9ce0157e20c648814838b3
YinBo <yinbo@Feinno.com>
2016-06-03 16:38:14 +0800

    图片

--- upload_img/routes/system.js
+++ upload_img/routes/system.js
@@ -125,7 +125,7 @@ router.post('/upload', function(req, res) {
     var req_origin = "*";
     res.setHeader('Access-Control-Allow-Origin', req_origin);
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST'); //POST, GET, PUT, DELETE, OPTIONS
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS'); //POST, GET, PUT, DELETE, OPTIONS
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept,X-Requested-With");
 
 
