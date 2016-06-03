e2a8a65460a058a44266600d337f0824e0a527d6
YinBo <yinbo@Feinno.com>
2016-06-03 16:29:33 +0800

    图片上传问题

--- upload_img/routes/system.js
+++ upload_img/routes/system.js
@@ -122,10 +122,10 @@ router.post('/upload', function(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
 */
 
-    var req_origin = req.headers.origin || "*";
+    var req_origin = "*";
     res.setHeader('Access-Control-Allow-Origin', req_origin);
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET'); //POST, GET, PUT, DELETE, OPTIONS
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST'); //POST, GET, PUT, DELETE, OPTIONS
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept,X-Requested-With");
 
 
