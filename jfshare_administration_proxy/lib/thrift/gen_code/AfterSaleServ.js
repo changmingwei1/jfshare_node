48400a75ff70a16b81775a6b279b05424d60fda3
huapengpeng <544523780@qq.com>
2016-06-16 16:06:45 +0800

    123

--- jfshare_administration_proxy/lib/thrift/gen_code/AfterSaleServ.js
+++ jfshare_administration_proxy/lib/thrift/gen_code/AfterSaleServ.js
@@ -10,8 +10,8 @@ var Q = thrift.Q;
 var result_ttypes = require('./result_types')
 var pagination_ttypes = require('./pagination_types')
 
-S
-var ttypes = require('./afterale_types');
+
+var ttypes = require('./afterSale_types');
 //HELPER FUNCTIONS AND STRUCTURES
 
 AfterSaleServ_request_args = function(args) {
