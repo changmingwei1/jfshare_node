7e750464f7383861ede3fb935b3ac3adcd5a8f99
huapengpeng <544523780@qq.com>
2016-06-23 12:37:47 +0800

    123

--- jfshare_administration_proxy/controllers/order_controller.js
+++ jfshare_administration_proxy/controllers/order_controller.js
@@ -281,7 +281,7 @@ router.post('/info', function (request, response, next) {
                         result.postage = orderInfo.postage;
                         result.sellerId = orderInfo.sellerId;
                         result.cancelTime = orderInfo.cancelTime;
-
+                        result.thirdScore = orderInfo.thirdScore;
                         if(orderInfo.orderState == 61){
                             result.cancelDesc = "其他原因";
                             if(orderInfo.orderStateType ==1){
