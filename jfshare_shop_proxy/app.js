e3885ea8e9a98d4af8a463a505c5c5f65d2cd4aa
YinBo <yinbo@Feinno.com>
2016-07-12 10:51:41 +0800

    node跨域问题

--- jfshare_shop_proxy/app.js
+++ jfshare_shop_proxy/app.js
@@ -47,13 +47,16 @@ app.use('/shop/active',active);
 // error handlers
 
 //设置跨域访问
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1');
-  res.header("Content-Type", "application/json;charset=utf-8");
-  next();
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (req.method == 'OPTIONS') {
+    res.send(200); /*让options请求快速返回*/
+  }
+  else {
+    next();
+  }
 });
 
 // catch 404 and forward to error handler
