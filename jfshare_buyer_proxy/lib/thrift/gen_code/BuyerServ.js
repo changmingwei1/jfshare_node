a7bb02c5306b34839b9f42daf47d3fb6698bb107
YinBo <yinbo@Feinno.com>
2016-08-08 15:03:36 +0800

    更新buyer的thrift文件

--- jfshare_buyer_proxy/lib/thrift/gen_code/BuyerServ.js
+++ jfshare_buyer_proxy/lib/thrift/gen_code/BuyerServ.js
@@ -1254,6 +1254,135 @@ BuyerServ_getBuyer_result.prototype.write = function(output) {
   return;
 };
 
+BuyerServ_getListBuyer_args = function(args) {
+  this.userIdList = null;
+  if (args) {
+    if (args.userIdList !== undefined) {
+      this.userIdList = args.userIdList;
+    }
+  }
+};
+BuyerServ_getListBuyer_args.prototype = {};
+BuyerServ_getListBuyer_args.prototype.read = function(input) {
+  input.readStructBegin();
+  while (true)
+  {
+    var ret = input.readFieldBegin();
+    var fname = ret.fname;
+    var ftype = ret.ftype;
+    var fid = ret.fid;
+    if (ftype == Thrift.Type.STOP) {
+      break;
+    }
+    switch (fid)
+    {
+      case 1:
+      if (ftype == Thrift.Type.LIST) {
+        var _size8 = 0;
+        var _rtmp312;
+        this.userIdList = [];
+        var _etype11 = 0;
+        _rtmp312 = input.readListBegin();
+        _etype11 = _rtmp312.etype;
+        _size8 = _rtmp312.size;
+        for (var _i13 = 0; _i13 < _size8; ++_i13)
+        {
+          var elem14 = null;
+          elem14 = input.readI32();
+          this.userIdList.push(elem14);
+        }
+        input.readListEnd();
+      } else {
+        input.skip(ftype);
+      }
+      break;
+      case 0:
+        input.skip(ftype);
+        break;
+      default:
+        input.skip(ftype);
+    }
+    input.readFieldEnd();
+  }
+  input.readStructEnd();
+  return;
+};
+
+BuyerServ_getListBuyer_args.prototype.write = function(output) {
+  output.writeStructBegin('BuyerServ_getListBuyer_args');
+  if (this.userIdList !== null && this.userIdList !== undefined) {
+    output.writeFieldBegin('userIdList', Thrift.Type.LIST, 1);
+    output.writeListBegin(Thrift.Type.I32, this.userIdList.length);
+    for (var iter15 in this.userIdList)
+    {
+      if (this.userIdList.hasOwnProperty(iter15))
+      {
+        iter15 = this.userIdList[iter15];
+        output.writeI32(iter15);
+      }
+    }
+    output.writeListEnd();
+    output.writeFieldEnd();
+  }
+  output.writeFieldStop();
+  output.writeStructEnd();
+  return;
+};
+
+BuyerServ_getListBuyer_result = function(args) {
+  this.success = null;
+  if (args) {
+    if (args.success !== undefined) {
+      this.success = args.success;
+    }
+  }
+};
+BuyerServ_getListBuyer_result.prototype = {};
+BuyerServ_getListBuyer_result.prototype.read = function(input) {
+  input.readStructBegin();
+  while (true)
+  {
+    var ret = input.readFieldBegin();
+    var fname = ret.fname;
+    var ftype = ret.ftype;
+    var fid = ret.fid;
+    if (ftype == Thrift.Type.STOP) {
+      break;
+    }
+    switch (fid)
+    {
+      case 0:
+      if (ftype == Thrift.Type.STRUCT) {
+        this.success = new ttypes.BuyerListResult();
+        this.success.read(input);
+      } else {
+        input.skip(ftype);
+      }
+      break;
+      case 0:
+        input.skip(ftype);
+        break;
+      default:
+        input.skip(ftype);
+    }
+    input.readFieldEnd();
+  }
+  input.readStructEnd();
+  return;
+};
+
+BuyerServ_getListBuyer_result.prototype.write = function(output) {
+  output.writeStructBegin('BuyerServ_getListBuyer_result');
+  if (this.success !== null && this.success !== undefined) {
+    output.writeFieldBegin('success', Thrift.Type.STRUCT, 0);
+    this.success.write(output);
+    output.writeFieldEnd();
+  }
+  output.writeFieldStop();
+  output.writeStructEnd();
+  return;
+};
+
 BuyerServ_updateBuyer_args = function(args) {
   this.buyer = null;
   if (args) {
@@ -3019,6 +3148,53 @@ BuyerServClient.prototype.recv_getBuyer = function(input,mtype,rseqid) {
   }
   return callback('getBuyer failed: unknown result');
 };
+BuyerServClient.prototype.getListBuyer = function(userIdList, callback) {
+  this._seqid = this.new_seqid();
+  if (callback === undefined) {
+    var _defer = Q.defer();
+    this._reqs[this.seqid()] = function(error, result) {
+      if (error) {
+        _defer.reject(error);
+      } else {
+        _defer.resolve(result);
+      }
+    };
+    this.send_getListBuyer(userIdList);
+    return _defer.promise;
+  } else {
+    this._reqs[this.seqid()] = callback;
+    this.send_getListBuyer(userIdList);
+  }
+};
+
+BuyerServClient.prototype.send_getListBuyer = function(userIdList) {
+  var output = new this.pClass(this.output);
+  output.writeMessageBegin('getListBuyer', Thrift.MessageType.CALL, this.seqid());
+  var args = new BuyerServ_getListBuyer_args();
+  args.userIdList = userIdList;
+  args.write(output);
+  output.writeMessageEnd();
+  return this.output.flush();
+};
+
+BuyerServClient.prototype.recv_getListBuyer = function(input,mtype,rseqid) {
+  var callback = this._reqs[rseqid] || function() {};
+  delete this._reqs[rseqid];
+  if (mtype == Thrift.MessageType.EXCEPTION) {
+    var x = new Thrift.TApplicationException();
+    x.read(input);
+    input.readMessageEnd();
+    return callback(x);
+  }
+  var result = new BuyerServ_getListBuyer_result();
+  result.read(input);
+  input.readMessageEnd();
+
+  if (null !== result.success) {
+    return callback(null, result.success);
+  }
+  return callback('getListBuyer failed: unknown result');
+};
 BuyerServClient.prototype.updateBuyer = function(buyer, callback) {
   this._seqid = this.new_seqid();
   if (callback === undefined) {
@@ -3841,6 +4017,36 @@ BuyerServProcessor.prototype.process_getBuyer = function(seqid, input, output) {
     this._handler.getBuyer(args.buyer,  function (err, result) {
       var result = new BuyerServ_getBuyer_result((err != null ? err : {success: result}));
       output.writeMessageBegin("getBuyer", Thrift.MessageType.REPLY, seqid);
+      result.write(output);
+      output.writeMessageEnd();
+      output.flush();
+    });
+  }
+}
+
+BuyerServProcessor.prototype.process_getListBuyer = function(seqid, input, output) {
+  var args = new BuyerServ_getListBuyer_args();
+  args.read(input);
+  input.readMessageEnd();
+  if (this._handler.getListBuyer.length === 1) {
+    Q.fcall(this._handler.getListBuyer, args.userIdList)
+      .then(function(result) {
+        var result = new BuyerServ_getListBuyer_result({success: result});
+        output.writeMessageBegin("getListBuyer", Thrift.MessageType.REPLY, seqid);
+        result.write(output);
+        output.writeMessageEnd();
+        output.flush();
+      }, function (err) {
+        var result = new BuyerServ_getListBuyer_result(err);
+        output.writeMessageBegin("getListBuyer", Thrift.MessageType.REPLY, seqid);
+        result.write(output);
+        output.writeMessageEnd();
+        output.flush();
+      });
+  } else {
+    this._handler.getListBuyer(args.userIdList,  function (err, result) {
+      var result = new BuyerServ_getListBuyer_result((err != null ? err : {success: result}));
+      output.writeMessageBegin("getListBuyer", Thrift.MessageType.REPLY, seqid);
       result.write(output);
       output.writeMessageEnd();
       output.flush();
