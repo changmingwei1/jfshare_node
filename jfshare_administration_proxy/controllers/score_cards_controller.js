60b4c22ab148c0797c70a8de0561b07fbcec1cdc
chiwenheng <826695301@qq.com>
2016-08-15 21:34:22 +0800

    change score excel funtion

--- jfshare_administration_proxy/controllers/score_cards_controller.js
+++ jfshare_administration_proxy/controllers/score_cards_controller.js
@@ -13,8 +13,8 @@ var logger = log4node.configlog4node.useLog4js(log4node.configlog4node.log4jsCon
 var ScoreCards = require('../lib/models/score_cards');// 积分卡功能
 
 router.post('/createOneActivity', function (request, response, next) {
-    logger.info("创建也给活动的流程");
-    var result = {code: 200};
+    logger.info("创建活动的流程");
+     var result = {code: 200};
     try {
         var params = request.body;
         //参数校验
@@ -79,6 +79,53 @@ router.post('/createOneActivity', function (request, response, next) {
             //     result.page = {total: pagination.totalCount, pageCount: pagination.pageNumCount};
             // }
             logger.info("createOneActivity result:" + JSON.stringify(data));
+            response.json(result);
+            return;
+        });
+    } catch (ex) {
+        logger.error("创建积分卡活动错误:" + ex);
+        result.code = 500;
+        result.desc = "创建积分卡活动错误";
+        response.json(result);
+    }
+});
+
+
+
+router.post('/exportExcelByqueryCards', function (request, response, next) {
+    logger.info("创建导出卡片excel的流程");
+    var result = {code: 200};
+    try {
+        var params = request.body;
+        //参数校验
+        logger.info("exportExcelByqueryCards params:" + JSON.stringify(params));
+        if(params.activityId ==null || params.activityId ==""){
+            result.code = 500;
+            result.desc = "参数错误";
+            response.json(result);
+            return;
+        }
+
+        if(params.psd ==null || params.psd ==""){
+            result.code = 500;
+            result.desc = "参数错误";
+            response.json(result);
+            return;
+        }
+
+
+        ScoreCards.exportExcelByqueryCards(params, function (err, data) {
+            if (err) {
+                response.json(err);
+                return;
+            }
+            // result.scoreList = data[0].scoreUsers;
+            // var pagination = data[0].pageination;
+            // if(pagination!=null){
+            //     result.page = {total: pagination.totalCount, pageCount: pagination.pageNumCount};
+            // }
+            logger.info("createOneActivity result:" + JSON.stringify(data));
+            result.path=data[0].path;
             response.json(result);
             return;
         });
