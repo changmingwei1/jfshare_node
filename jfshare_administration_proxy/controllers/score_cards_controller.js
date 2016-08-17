a9b0bc40822dc3464789eeb73153ead3c3d0f5a5
huapengpeng@feinno.com <huapengpeng>
2016-08-17 14:11:46 +0800

    批次列表查询

--- jfshare_administration_proxy/controllers/score_cards_controller.js
+++ jfshare_administration_proxy/controllers/score_cards_controller.js
@@ -1,5 +1,5 @@
 /**
- *  积分卡  管理 
+ *  积分卡  管理
  *  chiwenheng
  *  0815
  */
@@ -14,33 +14,33 @@ var ScoreCards = require('../lib/models/score_cards');// 积分卡功能
 
 router.post('/createOneActivity', function (request, response, next) {
     logger.info("创建活动的流程");
-     var result = {code: 200};
+    var result = {code: 200};
     try {
         var params = request.body;
         //参数校验
         logger.info("createOneActivity params:" + JSON.stringify(params));
-        if(params.name ==null || params.name ==""){
+        if (params.name == null || params.name == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
             return;
         }
 
-        if(params.startTime ==null || params.startTime ==""){
+        if (params.startTime == null || params.startTime == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
             return;
         }
 
-        if(params.endTime ==null || params.endTime ==""){
+        if (params.endTime == null || params.endTime == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
             return;
         }
 
-        if(params.multiRechargeEnable ==null || params.multiRechargeEnable ==""){
+        if (params.multiRechargeEnable == null || params.multiRechargeEnable == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
@@ -48,20 +48,20 @@ router.post('/createOneActivity', function (request, response, next) {
         }
 
 
-        if(params.pieceValue ==null || params.pieceValue ==""){
+        if (params.pieceValue == null || params.pieceValue == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
             return;
         }
 
-        if(params.totalCount ==null || params.totalCount ==""){
+        if (params.totalCount == null || params.totalCount == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
             return;
         }
-        if(params.rechargeType ==null || params.rechargeType ==""){
+        if (params.rechargeType == null || params.rechargeType == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
@@ -91,7 +91,6 @@ router.post('/createOneActivity', function (request, response, next) {
 });
 
 
-
 router.post('/exportExcelByqueryCards', function (request, response, next) {
     logger.info("创建导出卡片excel的流程");
     var result = {code: 200};
@@ -99,14 +98,14 @@ router.post('/exportExcelByqueryCards', function (request, response, next) {
         var params = request.body;
         //参数校验
         logger.info("exportExcelByqueryCards params:" + JSON.stringify(params));
-        if(params.activityId ==null || params.activityId ==""){
+        if (params.activityId == null || params.activityId == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
             return;
         }
 
-        if(params.psd ==null || params.psd ==""){
+        if (params.psd == null || params.psd == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
@@ -125,7 +124,7 @@ router.post('/exportExcelByqueryCards', function (request, response, next) {
             //     result.page = {total: pagination.totalCount, pageCount: pagination.pageNumCount};
             // }
             logger.info("createOneActivity result:" + JSON.stringify(data));
-            result.path=data[0].path;
+            result.path = data[0].path;
             response.json(result);
             return;
         });
@@ -144,7 +143,7 @@ router.post('/validataPsd', function (request, response, next) {
         var params = request.body;
         //参数校验
         logger.info("validataPsd params:" + JSON.stringify(params));
-        if(params.password ==null || params.password ==""){
+        if (params.password == null || params.password == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
@@ -174,19 +173,19 @@ router.post('/directionRecharge', function (request, response, next) {
         var params = request.body;
         //参数校验
         logger.info("directionRecharge params:" + JSON.stringify(params));
-        if(params.filePath ==null || params.filePath ==""){
+        if (params.filePath == null || params.filePath == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
             return;
         }
-        if(params.password ==null || params.password ==""){
+        if (params.password == null || params.password == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
             return;
         }
-        if(params.activityId ==null || params.activityId ==""){
+        if (params.activityId == null || params.activityId == "") {
             result.code = 500;
             result.desc = "参数错误";
             response.json(result);
@@ -208,5 +207,57 @@ router.post('/directionRecharge', function (request, response, next) {
         response.json(result);
     }
 });
+
+
+router.post('/getActivityList', function (request, response, next) {
+    logger.info("获取批次列表");
+    //设置默认值
+    var result = {code: 200};
+    result.activityList = [];
+    result.pagination = {
+        totalCount: 0,
+        pageNumCount: 0,
+        numPerPage: 0,
+        currentPage: 0
+    };
+    try {
+        var params = request.body;
+        if (params.numPerPage == null || params.numPerPage == "") {
+            result.code = 500;
+            result.desc = "参数错误";
+            response.json(result);
+            return;
+        }
+        if (params.currentPage == null || params.currentPage == "") {
+            result.code = 500;
+            result.desc = "参数错误";
+            response.json(result);
+            return;
+        }
+
+        ScoreCards.getActivityList(params, function (err, data) {
+            if (err) {
+                response.json(err);
+                return;
+            }
+
+            if (data != null && data[0] != null && data[0].activityList != null) {
+                result.activityList = data[0].activityList;
+            }
+            if (data != null && data[0] != null && data[0].pagination != null) {
+                result.pagination = data[0].pagination;
+            }
+            logger.info("getActivityList result:" + JSON.stringify(data));
+            response.json(result);
+            return;
+        });
+    } catch (ex) {
+        logger.error("获取批次列表失败:" + ex);
+        result.code = 500;
+        result.desc = "获取批次列表失败";
+        response.json(result);
+    }
+});
+
 
 module.exports = router;
