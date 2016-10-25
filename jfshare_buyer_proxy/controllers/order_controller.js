6ac48abe1931314dbaf9981d756f02ab70bb8e56
YinBo <yinbo@Feinno.com>
2016-10-25 10:23:56 +0800

    话费充值接口暂停

--- jfshare_buyer_proxy/controllers/order_controller.js
+++ jfshare_buyer_proxy/controllers/order_controller.js
@@ -1911,6 +1911,11 @@ router.post('/payOrderCreates', function (request, response, next) {
     logger.info("进入提交订单流程..");
     var result = {code: 200};
     try {
+        result.code = 500;
+        result.desc = "话费充值服务暂不可使用";
+        response.json(result);
+        return;
+
         var arg = request.body;
         if (arg == null || arg.userId == null || arg.sellerDetailList == null) {
             result.code = 400;
