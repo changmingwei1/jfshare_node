042fd9dd6abdbad497b592235c08919a166d590f
wuxiaoluo <2669033598@qq.com>
2016-11-02 11:26:37 +0800

    百分尊享新增活动页测试

--- jfshare_jf189/view_center/active/view_active.js
+++ jfshare_jf189/view_center/active/view_active.js
@@ -1,6 +1,5 @@
 exports.active = function(req, res, next) {
-    parameters.tyHostUrl = res.resData.tyHostUrl;
-    res.render("active/201611baoyou", res.paramters);
+    res.render("active/201611baoyou", res.resData);
 }
 
 
