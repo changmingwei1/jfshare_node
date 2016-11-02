8a9ea8c35d490558c319b1761d364c9b27968524
wuxiaoluo <2669033598@qq.com>
2016-11-02 11:23:10 +0800

    百分尊享新增活动页测试

--- jfshare_jf189/controllers/active_controller.js
+++ jfshare_jf189/controllers/active_controller.js
@@ -2,8 +2,6 @@
 var express = require('express');
 var router = express.Router();
 
-var logger = require('../lib/util/log4node').configlog4node.servLog4js();
-
 router.get('/', function(req, res, next) {
     res.render("active/view_active",res.resData);
 });
