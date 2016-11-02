02001e526ce208271e820a29d445d8c4c4c451cb
wuxiaoluo <2669033598@qq.com>
2016-11-02 10:51:23 +0800

    百分尊享新增活动页测试

---
+++ jfshare_jf189/controllers/active_controller.js
@@ -0,0 +1,12 @@
+//引入所需模块
+var express = require('express');
+var router = express.Router();
+
+var logger = require('../lib/util/log4node').configlog4node.servLog4js();
+
+router.get('/', function(req, res, next) {
+    res.render("active/view_active",res.resData);
+});
+
+//暴露模块
+module.exports = router;
---
+++ jfshare_jf189/view_center/active/view_active.js
@@ -0,0 +1,7 @@
+exports.active = function(req, res, next) {
+    res.render("active/201611baoyou", res.resData);
+}
+
+
+
+
