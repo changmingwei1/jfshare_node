f661570ce99861a33887b99118c6a91456074250
huapengpeng@feinno.com <huapengpeng>
2016-12-09 18:18:01 +0800

    暂时禁用Q币和游戏充值

--- jfshare_buyer_proxy/controllers/order_controller.js
+++ jfshare_buyer_proxy/controllers/order_controller.js
@@ -1944,6 +1944,14 @@ router.post('/payOrderCreates', function (request, response, next) {
         }
     }
     try {
+
+        if(arg.tradeCode == "Z8005" || arg.tradeCode == "Z8006"){
+            result.code = 500;
+            result.desc = "系统升级中";
+            response.json(result);
+            return;
+        }
+
         //if (arg.tradeCode == "Z8003") {
         //    result.code = 500;
         //    //result.desc = "话费充值服务暂不可使用";
