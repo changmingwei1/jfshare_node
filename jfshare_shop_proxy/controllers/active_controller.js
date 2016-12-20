02e4bf663cb4140c45d97dc184effc7a289d456a
YinBo <yinbo@Feinno.com>
2016-12-20 09:46:16 +0800

    update流量省份

--- jfshare_shop_proxy/controllers/active_controller.js
+++ jfshare_shop_proxy/controllers/active_controller.js
@@ -314,7 +314,7 @@ router.get('/queryMobileInfo',function(request,response,next){
     if (mobile.match(yd)) {
         var data = {};
         data.operator = "中国移动";
-        data.support = "仅限江苏、广东、浙江、山东移动用户充值";
+        data.support = "仅限江苏、山东、陕西、广东、浙江移动用户充值";
         //var flow1 = {flowName:"10M", flowno:"10", pieceValue:"3"};
         var flow2 = {flowName:"1G", flowno:"1024", pieceValue:"50"};
         var flow3 = {flowName:"2G", flowno:"2048", pieceValue:"70"};
@@ -331,7 +331,7 @@ router.get('/queryMobileInfo',function(request,response,next){
     } else if (mobile.match(lt)) {
         var data = {};
         data.operator = "中国联通";
-        data.support = "";
+        data.support = "仅限江西和上海联通用户充值";
         //var flow = {flowName:"20M", flowno:"20", pieceValue:"3"};
         var flow1 = {flowName:"500M", flowno:"500", pieceValue:"30"};
         flowList.push(flow1);
