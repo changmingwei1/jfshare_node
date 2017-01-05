3e94ded0332af441cc85e63946038c799876e8c5
wuxiaoluo <2669033598@qq.com>
2017-01-05 21:07:00 +0800

    百分尊享活动更新

--- jfshare_jf189/view_center/active/view_active.js
+++ jfshare_jf189/view_center/active/view_active.js
@@ -2,7 +2,7 @@ exports.baoyou = function(req, res, next) {
     res.render("active/201701shangpin", res.resData);
 };
 exports.christmas = function(req, res, next) {
-    res.render("active/201612shengdancha", res.resData);
+    res.render("active/201701xingzhan", res.resData);
 };
 
 
