2e05933a4ca5eb86e6d988455d1d1b2a6dea4b52
huapengpeng@feinno.com <huapengpeng>
2017-02-14 14:27:49 +0800

    线下扫毛加入一网通channel

--- jfshare_buyer_proxy/controllers/order_controller.js
+++ jfshare_buyer_proxy/controllers/order_controller.js
@@ -316,9 +316,14 @@ router.post('/afterSaleList', function (request, response, next) {
                                                     skuNum: order.productList[i].skuNum,
                                                     skuName: order.productList[i].skuDesc,
                                                     curPrice: order.productList[i].curPrice,
-                                                    imgKey: order.productList[i].imagesUrl.split(',')[0],
+
                                                     count: order.productList[i].count
                                                 };
+                                                if (order.productList[i].imagesUrl != null) {
+                                                    productItem.imgKey = order.productList[i].imagesUrl.split(',')[0];
+                                                } else {
+                                                    productItem.imgKey = "";
+                                                }
                                                 productList.push(productItem);
                                             }
                                             orderItem.productList = productList;
@@ -647,20 +652,19 @@ router.post('/list', function (request, response, next) {
                                             activeState: order.activeState,
                                             orderType: order.orderType,
                                             postage: order.postage,
-                                            timeOutLimit:0,//待支付超时时间
+                                            timeOutLimit: 0,//待支付超时时间
                                             type: order.productList[0].type  //5.17测没有type
                                         };
                                         //如果待支付
-                                        if(order.orderState ==10){
-                                            if(order.sellerId == 115){
-                                               orderItem.timeOutLimit = 10;
+                                        if (order.orderState == 10) {
+                                            if (order.sellerId == 115) {
+                                                orderItem.timeOutLimit = 10;
                                                 //  orderItem.timeOutLimit = 11;
-                                            }else{
+                                            } else {
                                                 orderItem.timeOutLimit = 2880;
                                                 // orderItem.timeOutLimit = 1440;
                                             }
                                         }
-
 
 
                                         if (order.deliverInfo != null) {
@@ -984,16 +988,15 @@ router.post('/info', function (req, res, next) {
                                 result.tradeCode = orderInfo.tradeCode;
 
                                 //如果待支付
-                                if(orderInfo.orderState ==10){
-                                    if(orderInfo.sellerId == 115){
+                                if (orderInfo.orderState == 10) {
+                                    if (orderInfo.sellerId == 115) {
                                         // result.timeOutLimit = 11;
                                         result.timeOutLimit = 10;
-                                    }else{
-                                       result.timeOutLimit = 2880;
+                                    } else {
+                                        result.timeOutLimit = 2880;
                                         //  result.timeOutLimit = 1440;
                                     }
                                 }
-
 
 
                                 //临时修改：因安卓没有62状态，所以62状态转换为61
@@ -1280,7 +1283,7 @@ router.post('/pay', function (req, res, next) {
                 res.json(result);
                 return;
             }
-        }else if (arg.payChannel == 11) {
+        } else if (arg.payChannel == 11) {
             try {
                 Order.payApply(arg, function (err, data) {
                     if (err) {
@@ -1289,7 +1292,7 @@ router.post('/pay', function (req, res, next) {
                     }
                     if (data !== null) {
 
-                        var payUrl = {"jsonRequestData":data.value};
+                        var payUrl = {"jsonRequestData": data.value};
                         //result.paramName = "jsonRequestData";
                         result.payUrl = payUrl;
                         result.action = "http://61.144.248.29:801/netpayment/BaseHttp.dll?MB_EUserPay";
@@ -1981,6 +1984,12 @@ router.post('/payOrderCreates', function (request, response, next) {
     logger.info("进入提交订单流程..");
     var result = {code: 200};
 
+
+    //result.code = 500;
+    // result.desc = "服务暂不可使用";
+    // response.json(result);
+    //return;
+
     //获取ip
     var ip1 = request.headers['x-real-ip'];
     /**
@@ -1994,7 +2003,7 @@ router.post('/payOrderCreates', function (request, response, next) {
     var arg = request.body;
 
     if (arg.tradeCode == "Z8006" || arg.tradeCode == "Z8005") {
-        if(arg.fromSource != 1){
+        if (arg.fromSource != 1) {
             arg.provinceName = ip1;
         }
     }
@@ -2007,12 +2016,18 @@ router.post('/payOrderCreates', function (request, response, next) {
         //    return;
         //}
 
-        if (arg.tradeCode == "Z8003") {
-           // result.code = 500;
+        if (arg.tradeCode == "Z8005") {
+
+            // result.code = 500;
+            // result.desc = "系统升级中";
+            // response.json(result);
+            // return;
+
+            // result.code = 500;
             //result.desc = "话费充值服务暂不可使用";
-           // result.desc = "运营商系统维护，话费充值服务暂不可用";
-           // response.json(result);
-          //  return;
+            // result.desc = "运营商系统维护，话费充值服务暂不可用";
+            // response.json(result);
+            //  return;
         }
         if (arg == null || arg.userId == null || arg.sellerDetailList == null) {
             result.code = 400;
@@ -2058,11 +2073,11 @@ router.post('/payOrderCreates', function (request, response, next) {
         }
         if (arg.tradeCode == "Z8003") { //话费
             if (arg.totalSum == "" || (
-                //arg.totalSum != "30" &&
-                arg.totalSum != "50" &&
-                arg.totalSum != "100" &&
-                arg.totalSum != "300" &&
-                arg.totalSum != "500"
+                    //arg.totalSum != "30" &&
+                    arg.totalSum != "50" &&
+                    arg.totalSum != "100" &&
+                    arg.totalSum != "300" &&
+                    arg.totalSum != "500"
                 )) {
                 result.code = 500;
                 result.desc = "该面值已售罄";
@@ -2070,17 +2085,17 @@ router.post('/payOrderCreates', function (request, response, next) {
                 return;
             }
             /*if (arg.totalSum == "30") {
-                result.code = 500;
-                result.desc = "该面值已售罄";
-                response.json(result);
-                return;
-            }
-            if (arg.totalSum == "50") {
-                result.code = 500;
-                result.desc = "该面值已售罄";
-                response.json(result);
-                return;
-            }*/
+             result.code = 500;
+             result.desc = "该面值已售罄";
+             response.json(result);
+             return;
+             }
+             if (arg.totalSum == "50") {
+             result.code = 500;
+             result.desc = "该面值已售罄";
+             response.json(result);
+             return;
+             }*/
         }
         if (arg.tradeCode == "Z8004") { //流量
             //result.code = 500;
