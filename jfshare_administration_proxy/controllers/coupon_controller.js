9d8c59e8514c89d87f163ce997ca2748309a6dc7
huapengpeng@feinno.com <huapengpeng>
2017-05-15 17:12:42 +0800

    修改

--- jfshare_administration_proxy/controllers/coupon_controller.js
+++ jfshare_administration_proxy/controllers/coupon_controller.js
@@ -7,8 +7,8 @@ var router = express.Router();
 var log4node = require('../log4node');
 var logger = log4node.configlog4node.useLog4js( log4node.configlog4node.log4jsConfig);
 
-var Product = require('../lib/models/product');
-var Address = require('../lib/models/address');
+//var Product = require('../lib/models/product');
+//var Address = require('../lib/models/address');
 var page = require('../lib/thrift/gen_code/pagination_types.js');
 //
 router.post('/couponList', function(req, res, next) {
